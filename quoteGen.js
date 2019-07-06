e150f4066d5cfbb1235c46e4ad5c14dc08e01ca8
Princewill <okechukwuprincewillw@gmail.com>
2019-07-06 12:23:39 +0100

    final

---
+++ quoteGen.js
@@ -0,0 +1,26 @@
+var arrayOfQuotes = [
+  {
+    author: "Jim Rohn",
+    quote: "Beware of what you become in pursuit of what you want."
+  },
+  {
+    author: "Epictetus",
+    quote: "Its not what happens to you, but how youreact to it that matters."
+  },
+  { author: "Frank Sinatra", quote: "The best revenge is massive success" },
+  {
+    author: "Bishop David Oyedepo",
+    quote: "you are responsible for the outcome of your life"
+  }
+];
+function randomSelector(array) {
+  return Math.floor(Math.random() * array.length);
+}
+
+function generateQuote() {
+  var randomNumber = randomSelector(arrayOfQuotes);
+  document.getElementById("quoteOutput").innerHTML =
+    '"' + arrayOfQuotes[randomNumber]["quote"] + '"';
+  document.getElementById("authorOutput").innerHTML =
+    "-" + arrayOfQuotes[randomNumber]["author"];
+}
